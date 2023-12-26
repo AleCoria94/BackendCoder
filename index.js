c2d3ab00b172f611aebd5d7fecc285294a16a328
Alexis Coria <coria.alexis94@gmail.com>
2023-12-25 23:33:46 -0300

    2da entrega

--- index.js
+++ index.js
@@ -1,41 +1,84 @@
+const fs= require('fs')
+const { stringify } = require('querystring')
+
 class ProductManager {
     constructor() {
         this.products = [];
-        this.nextProductId = 1;
+        this.newId = 1;
+        this.path = './products.json'
     }
 
-    addProduct(product) {
-        if (!this.isProductValid(product)) {
-            console.log("Por favor, ingrese los datos correctamente");
-            return;
-        }
+    async addProduct(product) {
+        try{
+            if(!this.isProductValid(product)){
+                console.log("Por favor, complete todos los datos")
+            return
+            }
+            if(this.isCodeDuplicated(product.code)){
+                console.log("Error. Ya está duplicado el codigo", product.code, ". Por favor, ingrese otro codigo")
+            return
+            }
+            const products = await this.getProducts();
 
-        if (this.isCodeDuplicate(product.code)) {
-            console.error("El codigo ya existe. Por favor ingrese uno nuevo.");
-            return;
-        }
-
-        const newProduct = {
-            ...product,
-            id: this.nextProductId
-        };
-
-        this.products.push(newProduct);
-        this.nextProductId++;
-    }
-
-    getProducts() {
-        return this.products;
-    }
-
-    getProductById(id) {
-        const product = this.products.find(product => product.id === id);
-        if (product) {
-            return product;
-        } else {
-            console.error("Product not found");
+            const newProduct = {id:this.newId,...product}
+            products.push(newProduct);
+            this.newId++
+            await fs.promises.writeFile(this.path,JSON.stringify(products))
+        }catch(error){
+            console.log('Este es el error',error)
         }
     }
+    async getProducts() {
+            try{
+                if(fs.existsSync(this.path)){
+                    const infoJSON = await fs.promises.readFile(this.path,'utf-8');
+                    const infoJS = JSON.parse(infoJSON);
+                    return infoJS;
+                }else return []
+            }catch(error){
+                console.log(error)
+            }
+        }
+    
+
+        async getProductById(id){
+            try{
+                const products = await this.getProducts();
+                const product = products.find(p=>p.id===id)
+                    if(product){
+                    return product
+                            } else{
+                            
+                                console.log("Producto no encontrado")
+                                }   
+                            }
+            catch(error){
+                    console.log(error)
+                        }
+        }
+
+        async updatedId(id, updatedproduct){
+            try{
+                const products = await this.getProducts();
+            const indice = products.findIndex(p=>p.id===id)
+            if (indice === -1) {
+                console.log('Producto no encontrado con id', id);
+                return;
+              } 
+    
+            }catch(error){
+                console.log(error)
+            }
+        }
+        async deleteById(id){
+            try{
+            const products = await this.getProducts();
+            const newProducts = products.filter(p=>p.id!==id)
+            await fs.promises.writeFile(this.path,JSON.stringify(newProducts))
+            }catch(error){
+                console.log(error);
+            }
+        }
 
     isProductValid(product) {
         return (
@@ -48,7 +91,7 @@ class ProductManager {
         );
     }
 
-    isCodeDuplicate(code) {
+    isCodeDuplicated(code) {
         return this.products.some(product => product.code === code);
     }
 }
@@ -56,49 +99,14 @@ class ProductManager {
 // Ejemplo de uso
 const productManager = new ProductManager();
 
-productManager.addProduct({
-    title: "Lavarropas Semi",
-    description: "Lavarropas Semi Automatico 6kg Carga frontal",
-    price: 109999,
-    thumbnail: "ruta1",
-    code: "LAVA6CF",
-    stock: 10
-});
-
-productManager.addProduct({
-    title: "Auriculares Desi",
-    description: "Auriculares Cable 3 metros Design",
-    price: 59999,
-    thumbnail: "ruta2",
-    code: "AURIDESI3M",
-    stock: 5
-});
-
-productManager.addProduct({
-    title: "Heladera XXL",
-    description: "Heladera Combi XXl 440 litros",
-    price: 319999,
-    thumbnail: "ruta3",
-    code: "HELAXXL440",
-    stock: 1
-});
-/* Se agrega un ejemplo donde se repita el code pero que los demas atributos  */
-productManager.addProduct({
-    title: "Heladera",
-    description: "Heladera negra",
-    price: 319999,
-    thumbnail: "ruta3",
-    code: "HELAXXL440",
-    stock: 1
-});
-
-console.log("----------------------------")
-console.log(productManager.getProducts());
-
-
-console.log("-----------prodById---------------")
-const productById = productManager.getProductById(3);
-console.log(productById);
-console.log("----------------------------")
-const productByIdNotFound = productManager.getProductById(5);
-console.log("----------------------------")
+const test =async()=>{
+    await productManager.addProduct({title:'heladera',description:'Esto es una heladera',price:1999999,thumbnail:'ruta',code:'0001',stock:15})
+    console.log('Lista de productos. Primer intento',await productManager.getProducts());
+    await productManager.addProduct({title:'lavarropas',description:'Esto es un lavarropas',price:2888888,thumbnail:'ruta1',code:'0002',stock:3})
+    console.log('Lista de productos. Segundo intento',await productManager.getProducts());
+    console.log('Esto es un consulta por Id 1',await productManager.getProductById(1));
+    console.log('Prueba de codigo repetido:', await productManager.addProduct({title:'producto prueba',description:'Este es un producto prueba',price:200,thumbnail:'Sin imagen',code:'abc123',stock:25}));
+    console.log('Esto es la eliminacion del id: 1 ',await productManager.deleteById(1));
+    console.log('Lista de productos. Tercer intento',await productManager.getProducts());
+    }
+    test();
